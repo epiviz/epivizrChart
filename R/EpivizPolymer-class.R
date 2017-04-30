a37d0632a7619bc9dc68034a01031b0ed0e1c0db
Brian Gottfried <briangottfried@me.com>
2017-04-30 16:12:01 -0400

    Show function change

--- R/EpivizPolymer-class.R
+++ R/EpivizPolymer-class.R
@@ -114,7 +114,7 @@ EpivizPolymer <- setRefClass("EpivizPolymer",
     },
     show = function() {
       "Show environment of this object"
-      return(.self$epiviz_envir)
+      .self$epiviz_envir
     }
   )
 )
