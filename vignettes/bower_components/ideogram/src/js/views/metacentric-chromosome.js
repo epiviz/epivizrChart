8a1d19062bc31271198d3806fc9df63bb1739eef
Brian Gottfried <briangottfried@me.com>
2017-06-14 15:03:13 -0400

    Removed unwanted files

--- vignettes/bower_components/ideogram/src/js/views/metacentric-chromosome.js
+++
@@ -1,6 +0,0 @@
-function MetacentricChromosome(model, config, ideo) {
-  Chromosome.call(this, model, config, ideo);
-  this._class = 'MetacentricChromosome';
-}
-
-MetacentricChromosome.prototype = Object.create(Chromosome.prototype);
